27a9f7e47a4b879e14bfbd31c05bd5befa59291f
Mike Wilensky <mjw768@uw.edu>
2020-04-10 10:50:57 -0700

    Make time-frequency selection more robust

--- scripts/occ_csv.py
+++ scripts/occ_csv.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--csv', help='A csv whose columns are obsids, ins data files, ins masks, and yml files')
     parser.add_argument('-o', '--outfile', help='The name of the output csv that contains occupancy information')
     parser.add_argument('-i', '--ch_ignore', help='A text file of fine frequency channels to ignore in occupancy calculation')
-    parser.add_argument('-t', '--time_ignore', help='Times to ignore when calculating occupancy')
+    parser.add_argument('-t', '--time_ignore', help='Time indices to ignore when calculating occupancy')
     args = parser.parse_args()
 
     dict_list = csv_to_dict_list(args.csv)
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     if args.time_ignore is not None:
         time_ignore = util.make_obslist(args.time_ignore)
-        time_ignore = np.array(time_ignore).astype(float)
+        time_ignore = np.array(time_ignore).astype(int)
     else:
         time_ignore = None
 
@@ -59,10 +59,27 @@ if __name__ == "__main__":
             freq_chans = np.arange(len(ins.freq_array))
             freq_chans = np.delete(freq_chans, fine_channels_ignore)
             ins.select(freq_chans=freq_chans)
+            # Also have to remove these freqs from match_events
+            new_events = []
+            for event in ins.match_events:
+                if (event[1].start in fine_channels_ignore) and ((event[2] == 'narrow') or (event[2] == 'samp_thresh')):
+                    continue
+                else:
+                    new_events.append(event)
+            ins.match_events = new_events
+
         if time_ignore is not None:
             times = ins.time_array
-            times = np.delete(times, time_ignore)
+            times = np.delete(times, times[time_ignore])
             ins.select(times=times)
+            # Also have to remove these times from match_events
+            new_events = []
+            for event in match_events:
+                if event[0] in time_ignore:
+                    continue
+                else:
+                    new_events.append(event)
+            ins.match_events = new_events
 
         total_occ = np.mean(ins.metric_array.mask[:, :, 0])
 
